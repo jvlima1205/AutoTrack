5f68aa8ea123aff4d95a1f8da486a1979524e483
jvlima1205 <jvlima1205@gmail.com>
2026-03-06 15:48:56 -0300

    Tratamento de erro
    Caso ocorra algum erro no sistema, será redirecionada para a pagina de erro com os detalhes da exception

---
+++ AutoTrack.Blazor/Classes/Redirect.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AutoTrack.Blazor.Classes;
+
+public class Redirect(NavigationManager Nav)
+{
+    public void Error(Exception ex)
+    {
+        Nav.NavigateTo($"/Error?errormessage={ex.Message}&innererror={ex.InnerException}&details={ex}");
+    }
+}
